4413fc05a878de25363d96dcd0da354014095d18
charbelbkhalil <102141925+charbelbkhalil@users.noreply.github.com>
2023-03-22 11:41:39 +0200

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "@cryptogate/react-ui/dist/esm/index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>SOLTHAW-IO</title>
+      </Head>
+
+        <Component {...pageProps} />
+    
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
